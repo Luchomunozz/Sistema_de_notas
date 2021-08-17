30771a1044952a211a9b0c7e7fd780c8869978ff
unknown <SENA@ARMAPRRQDFSD314.FormacionSena.RED>
2021-08-17 13:04:49 -0500

    L_1.0_esquemageneral

---
+++ Ejercicio 1.py
@@ -0,0 +1,6 @@
+Nota1 = int(input("Ingrese la primera nota: "))
+Nota2 = int(input("Ingrese la segunda nota: "))
+Nota3 = int(input("Ingrese la tercera nota: "))
+Nota4 = int(input("Ingrese la cuarta nota: "))
+Nota5 = int(input("Ingrese la quinta nota: "))
+
