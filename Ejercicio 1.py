38488122b7505b4ccefc44e0b3fb7aac9fe29f34
camilo3312 <ccvelasquez531@misena.edu.co>
2021-08-18 17:23:48 -0500

    CC_1.1 se validaron los ingresos de numeros

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -1,6 +1,31 @@
-Nota1 = int(input("Ingrese la primera nota: "))
+def Validar(nota):
+    try: 
+        int(nota)
+        return True
+
+    except ValueError:
+        print("Ingrese un numero entero")
+
+        return False
+
+
+Nota1 = input("Ingrese la primera nota: ")
+Validar(Nota1)
+
 Nota2 = int(input("Ingrese la segunda nota: "))
+Validar(Nota2)
+
 Nota3 = int(input("Ingrese la tercera nota: "))
+Validar(Nota3)
+
 Nota4 = int(input("Ingrese la cuarta nota: "))
+Validar(Nota4)
+
 Nota5 = int(input("Ingrese la quinta nota: "))
+Validar(Nota5)
+
+
+
+
+
 
