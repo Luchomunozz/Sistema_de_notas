f04856fdfac0ac5468683f6321a89386c7d05052
= <=>
2021-08-19 12:18:12 -0500

    Se resolvio errores

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -1,28 +1,25 @@
 def Validar(nota):
     try: 
         int(nota)
-        return True
-
+       
     except ValueError:
         print("Ingrese un numero entero")
-
-        return False
-
-
+  
 Nota1 = input("Ingrese la primera nota: ")
 Validar(Nota1)
 
-Nota2 = int(input("Ingrese la segunda nota: "))
+Nota2 = input("Ingrese la segunda nota: ")
 Validar(Nota2)
 
-Nota3 = int(input("Ingrese la tercera nota: "))
+Nota3 = input("Ingrese la tercera nota: ")
 Validar(Nota3)
 
-Nota4 = int(input("Ingrese la cuarta nota: "))
+Nota4 = input("Ingrese la cuarta nota: ")
 Validar(Nota4)
 
-Nota5 = int(input("Ingrese la quinta nota: "))
+Nota5 = input("Ingrese la quinta nota: ")
 Validar(Nota5)
+
 
 
 
