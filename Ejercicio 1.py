70bafc5b9b99d87ea47921a0b6668daeec69309b
mari293 <maricruzg020903@gmail.com>
2021-08-18 21:56:26 -0500

    M_1.0_Se capturan los datos del estudiante

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -4,3 +4,11 @@ Nota3 = int(input("Ingrese la tercera nota: "))
 Nota4 = int(input("Ingrese la cuarta nota: "))
 Nota5 = int(input("Ingrese la quinta nota: "))
 
+#Se piden y capturan los datos del estudiante
+def capturar_datos():
+    nombre = input("Ingrese el nombre del estudiante: ")
+    apellidos = input("Ingrese el apellidos del estudiante: ")
+    grado = input("Ingrese el grado del estudiante: ")
+    materia = input("Ingrese la materia: ")
+    
+    return nombre, apellidos, grado, materia
