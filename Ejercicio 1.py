3b8c05eb87ba345a302047dc72c9d39ac8d9e01e
David Torres <dmtg16@hotmail.com>
2021-08-18 21:52:20 -0500

    D_1.0_Se creó el ponderado y se completó la función

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -1,6 +1,29 @@
-Nota1 = int(input("Ingrese la primera nota: "))
-Nota2 = int(input("Ingrese la segunda nota: "))
-Nota3 = int(input("Ingrese la tercera nota: "))
-Nota4 = int(input("Ingrese la cuarta nota: "))
-Nota5 = int(input("Ingrese la quinta nota: "))
+Nota1 = float(input("Ingrese la primera nota: "))
+Nota2 = float(input("Ingrese la segunda nota: "))
+Nota3 = float(input("Ingrese la tercera nota: "))
+Nota4 = float(input("Ingrese la cuarta nota: "))
+Nota5 = float(input("Ingrese la quinta nota: "))
+
+
+def CalcularNota(Nota1,Nota2,Nota3,Nota4,Nota5):
+
+    Nota1= Nota1*0.10
+    print(Nota1)
+    Nota2= Nota2*0.10
+    print(Nota2)
+    Nota3= Nota3*0.15
+    print(Nota3)
+    Nota4= Nota4*0.20
+    print(Nota4)
+    Nota5= Nota5*0.45
+    print(Nota5)
+
+    ponderado= (Nota1+Nota2+Nota3+Nota4+Nota5)
+
+    return ponderado
+
+
+CalcularNota(Nota1,Nota2,Nota3,Nota4,Nota5)
+
+
 
